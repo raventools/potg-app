b6c6af7879d135f656faa18a5e3157a53277d49d
Phil Dier <phil@raventools.com>
2016-02-23 12:56:12 -0600

    add metadata for recipes, version bump

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,11 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures potg-app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "apache2"
 depends "raven-php"
 depends "raven-deploy"
+
+recipe "potg-app::default", "set up potg dependencies"
+recipe "potg-app::deploy_tag", "deploys potg app"
